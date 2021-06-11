493e88a40ffcc68d57701dc5b757c21cd7f73389
icedterminal <icedterminal@gmail.com>
2021-06-11 18:53:59 +0000

    add numeric pagination

--- nav-below.php
+++ nav-below.php
@@ -1,5 +1,6 @@
-<?php $args = array(
-'prev_text' => sprintf( esc_html__( '%s older', 'icx' ), '<span class="meta-nav">&larr;</span>' ),
-'next_text' => sprintf( esc_html__( 'newer %s', 'icx' ), '<span class="meta-nav">&rarr;</span>' )
-);
-the_posts_navigation( $args ); ?>
+<?php //$args = array(
+    //'next_text' => sprintf( esc_html__( 'newer %s', 'icx' ), '<span class="meta-nav">&rarr;</span>' ),
+    //'prev_text' => sprintf( esc_html__( '%s older', 'icx' ), '<span class="meta-nav">&larr;</span>' )
+//);
+//the_posts_navigation( $args ); ?>
+<?php numeric_pagi_nav(); ?>
