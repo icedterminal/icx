86d50ff32fc9c4c0e246134d377868598cd9ff69
icedterminal <icedterminal@gmail.com>
2022-05-10 01:17:03 -0700

    Formatting

--- front-page.php
+++ front-page.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
-    <main id="content" class="full">
-        <article id="landing">
-            <div class="logo">
-            </div>
-            <div class="entry-content-full">
-                <?php the_content(); ?>
-            </div>
-        </article>
-    </main>
+<main id="content" class="full">
+    <article id="landing">
+        <div class="logo">
+        </div>
+        <div class="entry-content-full">
+            <?php the_content(); ?>
+        </div>
+    </article>
+</main>
 <?php get_footer(); ?>
